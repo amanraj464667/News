95fc4fce6eb1cd613774920d9aa149f12f4b3b74
Aman <rajaman@gmail.com>
2024-12-07 16:26:17 +0530

    commit1

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -5,8 +5,8 @@ const NewsItem = ({title, description, src, url}) => {
     <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
   <img src={src?src:image} style={{height:"200px",width:"100%", objectFit:"cover"}} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title.slice(0,50)}</h5>
-    <p className="card-text">{description?description.slice(0,90):"News is information about current events. It is information about something that has just happened."}</p>
+  <h5 className="card-title">{title  ?title.slice(0, 40) : "Breaking news"}</h5>
+  <p className="card-text">{description ? description.slice(0, 90) : "Updates: What is going on today, Check out here."}</p>
     <a href={url} className="btn btn-primary">Read More</a>
   </div>
 </div>
